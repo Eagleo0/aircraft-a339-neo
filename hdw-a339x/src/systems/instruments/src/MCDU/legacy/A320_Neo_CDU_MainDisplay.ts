1e1b7e3bb2e4d423f0244785d027f4f58e68df32
Denis Freund <info@denis-freund.de>
2026-07-11 23:12:52 +0200

    fix(mcdu): hide some atc menu options based on cpdlc status
    FBW PR 10153

--- hdw-a339x/src/systems/instruments/src/MCDU/legacy/A320_Neo_CDU_MainDisplay.ts
+++ hdw-a339x/src/systems/instruments/src/MCDU/legacy/A320_Neo_CDU_MainDisplay.ts
@@ -197,8 +197,15 @@ export class A320_Neo_CDU_MainDisplay
     AOCFreeText: 76,
     StepAltsPage: 77,
     ATCDepartReq: 78,
-    RTAPage: 79,
-    AcarsMenuPage: 80,
+    ATCCommMenu: 79,
+    ATCText: 80,
+    ATCProcedureRequest: 81,
+    ATCVertRequest: 82,
+    ATCLatRequest: 83,
+    ATCMessageModifyUM131: 84,
+    ATCContactRequest: 85,
+    RTAPage: 86,
+    AcarsMenuPage: 87,
   };
 
   private mcduServerClient?: McduServerClient;
